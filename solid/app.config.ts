4184cc26dce8614ea2a4d167e6505cfcd21f0ff0
Nikita <nikita.voloboev@gmail.com>
2024-03-18 14:05:36 +0400

    .

--- solid/app.config.ts
+++ solid/app.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config"
 
-export default defineConfig({})
+export default defineConfig({
+  server: {
+    preset: "cloudflare_pages",
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
+  },
+})
