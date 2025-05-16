2a9cac45f9912af1682d2eb36cbe483a43bb03e7
Nikita <github@nikiv.dev>
2025-05-16 20:26:47 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ export default function page() {
   return (
     <div className="w-full py-4 px-2 flex flex-row h-screen bg-[#0f0f0f]">
       <Sidebar />
-      <div className="w-full h-full bg-[#161616] border border-white/10 rounded-xl p-4">
+      {/* <div className="w-full h-full bg-[#161616] border border-white/10 rounded-xl p-4">
         <Main />
-      </div>
+      </div> */}
     </div>
   )
 }
