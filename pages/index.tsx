5d32bf03392d3afd2fab9b52466defcdfbc59332
Nikita <github@nikiv.dev>
2025-05-16 20:41:00 +0200

    better hmr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Sidebar from "../components/sidebar"
-import Main from "../components/main"
+import ExampleToggle from "../components/toggle"
 
 export default function page() {
   return (
     <div className="w-full py-4 px-2 flex flex-row h-screen bg-[#0f0f0f]">
-      <Sidebar />
-      {/* <div className="w-full h-full bg-[#161616] border border-white/10 rounded-xl p-4">
-        <Main />
-      </div> */}
+      <ExampleToggle />
     </div>
   )
 }
